e70b82cf9f809ed2f3ed66cd53e85e9e1398cd76
Ken Tran <kentran71505@gmail.com>
2025-09-24 21:41:15 -0400

    smoothing has been completed

--- src/detect_pose.py
+++ src/detect_pose.py
@@ -2,15 +2,24 @@ def run_pose(video_path, weights='yolov8s-pose.pt'):
     from ultralytics import YOLO
     import numpy as np
 
-    model = YOLO(weights) 
-    xy, cf = [], []
-    for res in model.track(source=video_path, stream=True, verbose=False, persist=True):
-        if res.keypoints is None or len(res.keypoints) == 0:
-            #deals with frames where a keypoint cant be detected
-            xy.append(np.full((17,2), np.nan, np.float32))
-            cf.append(np.zeros(17, np.float32))
+    model = YOLO(weights)
+    results = model(video_path)
+
+    xy = []     #Stores x-y coordinates
+    con = []    #Stores confidence of those coordinates
+
+    for frame in results:
+        #Dealing with frames where keypoints cant be detected. 
+        if frame.keypoints is None or len(frame.keypoints) == 0:
+            xy.append(np.full((17,2), np.nan, dtype=np.float32))
+            con.append(np.zeros((17, 0), dtype= np.float32))
             continue
-        i = int(res.boxes.conf.argmax())  # i is "person" w highest confidence rating
-        xy.append(res.keypoints.xy[i].cpu().numpy().astype('float32'))
-        cf.append(res.keypoints.conf[i].cpu().numpy().astype('float32'))
-    return np.stack(xy), np.stack(cf)
+
+        #Case where frames are detectable
+        person_idx = int(frame.boxes.conf.argmax().item())
+        xy.append(frame.keypoints.xy[person_idx].cpu().numpy().astype(np.float32))
+        con.append(frame.keypoints.conf[person_idx].cpu().numpy().astype(np.float32))
+
+    return np.stack(xy), np.stack(con)
+
+            
