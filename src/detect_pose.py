572ed71e5b1201e3b3913d5bcf7c1a3b3836ea3d
Ken Tran <kentran71505@gmail.com>
2025-10-16 11:24:00 -0400

    update?

--- src/detect_pose.py
+++ src/detect_pose.py
@@ -3,22 +3,23 @@ def run_pose(video_path, weights='yolov8s-pose.pt'):
     import numpy as np
 
     model = YOLO(weights)
-    results = model(video_path)
+    results = model(video_path, stream = True)
 
-    xy = []     #Stores x-y coordinates
-    con = []    #Stores confidence of those coordinates
-
+    xy = []
+    con = []
     for frame in results:
         #Dealing with frames where keypoints cant be detected. 
         if frame.keypoints is None or len(frame.keypoints) == 0:
             xy.append(np.full((17,2), np.nan, dtype=np.float32))
-            con.append(np.zeros((17, 0), dtype= np.float32))
+            con.append(np.zeros((17,), dtype= np.float32))
             continue
 
-        #Case where frames are detectable
+        #Normal case
         person_idx = int(frame.boxes.conf.argmax().item())
         xy.append(frame.keypoints.xy[person_idx].cpu().numpy().astype(np.float32))
-        con.append(frame.keypoints.conf[person_idx].cpu().numpy().astype(np.float32))
+
+        if frame.keypoints.conf == None:
+            con.append(frame.keypoints.conf[person_idx].cpu().numpy().astype(np.float32))
 
     return np.stack(xy), np.stack(con)
 
