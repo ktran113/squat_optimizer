0c6c6e0b315b9c419ef46e5577f0c86b4fb5facd
Ken Tran <kentran71505@gmail.com>
2025-11-23 05:55:49 -0500

    update

--- src/barbell_detection.py
+++ src/barbell_detection.py
@@ -3,12 +3,17 @@ import cv2
 import numpy as np
     
 def run_detection(path, api_key, project_name, version):
+    """
+    Takes care of loading the model and returns the results as
+    a tuple per frame of x,y coordinates and the confidence rating
+    in an np.array
+    """
     print("loading model")
     rf = roboflow.Roboflow(api_key= api_key)
     project = rf.workspace().project(project_name)
     model = project.version(version).model
 
-    cap = cv2.videoCapture(path)
+    cap = cv2.VideoCapture(path)
     if not cap.isOpened():
         raise ValueError("Video could not be opened")
 
@@ -21,23 +26,25 @@ def run_detection(path, api_key, project_name, version):
         ret, frame = cap.read()
         if not ret:
             break 
-        barbell.append(detect_frame(model, cap))
+        barbell.append(detect_frame(model, frame))
     
     cap.release()
     print("Done")
 
-    return np.array(barbell)
-
-
+    return np.array(barbell, dtype=np.float32)
 
 def detect_frame (model, frame):
+    """
+    Analyzes individual frame and returns tuple consisting
+    of that frames x,y cordinates and their confidence rating
+    """
     path = "temp.jpg"
     cv2.imwrite(path, frame)
 
     prediction = model.predict(path, confidence=50).json()
 
-    if prediction['predicitons']:
-        best = max(prediction["predicitons"], key=lambda x: x["confidence"])
+    if prediction['predictions']:
+        best = max(prediction["predictions"], key=lambda x: x["confidence"])
         x_cords = best["x"]
         y_cords = best["y"]
         con = best["confidence"]
--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 def main():
-
+    print("running")
 
 if __name__ == "__main__":
     main()
